2edc763f036baab5664799a41ea4f0908d836b12
Riyad M Salem <64443244+riyadsalem@users.noreply.github.com>
2022-07-10 18:00:08 +0300

    Update telegram.php

--- config/telegram.php
+++ config/telegram.php
@@ -38,8 +38,6 @@ return [
             'certificate_path'    => env('TELEGRAM_CERTIFICATE_PATH', ''),
             'webhook_url'         => env('TELEGRAM_WEBHOOK_URL', ''),
             'commands'            => [
-                //Acme\Project\Commands\MyTelegramBot\BotCommand::class
-                //https://api.telegram.org/bot5436860040:AAFITA0xGOzgBcmyb1Ghv7HznfempgqBg4g/getMe
             ],
         ],
 
